a93cdd52e113f75a5dbc94346d45d4ff89e5b00e
joao <joaocfariaf@gmail.com>
2019-06-25 18:50:24 -0300

    Updating visual

--- FrontEnd/User/src/components/dashboard/RecentlyProducts.js
+++ FrontEnd/User/src/components/dashboard/RecentlyProducts.js
@@ -41,7 +41,7 @@ const RecentlyProducts = (props) => {
   return (
     <Paper>
       <List>
-        <Subheader style={styles.subheader}>Recent Products</Subheader>
+        <Subheader style={styles.subheader}>Produtos Recentes</Subheader>
         {props.data.map(item =>
           <div key={item.title}>
             <ListItem
@@ -50,7 +50,7 @@ const RecentlyProducts = (props) => {
               secondaryText={item.text}
               rightIconButton={rightIconMenu}
             />
-            <Divider inset={true} />
+            <Divider inset={false} />
           </div>
         )}
       </List>
--- FrontEnd/User/src/containers/DashboardPage.js
+++ FrontEnd/User/src/containers/DashboardPage.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import {cyan600, pink600, purple600, orange600} from 'material-ui/styles/colors';
-import Assessment from 'material-ui/svg-icons/action/assessment';
-import Face from 'material-ui/svg-icons/action/face';
-import ThumbUp from 'material-ui/svg-icons/action/thumb-up';
-import ShoppingCart from 'material-ui/svg-icons/action/shopping-cart';
+import {pink600, purple600, orange600} from 'material-ui/styles/colors';
+import Assessment from 'material-ui/svg-icons/action/card-giftcard';
+import Car from 'material-ui/svg-icons/maps/directions-car';
+import Coffee from 'material-ui/svg-icons/maps/local-cafe';
 import InfoBox from '../components/dashboard/InfoBox';
-import NewOrders from '../components/dashboard/NewOrders';
-import MonthlySales from '../components/dashboard/MonthlySales';
-import BrowserUsage from '../components/dashboard/BrowserUsage';
 import RecentlyProducts from '../components/dashboard/RecentlyProducts';
 import globalStyles from '../styles';
 import Data from '../data';
@@ -21,19 +17,16 @@ const DashboardPage = () => {
       <div className="row">
 
         <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3 m-b-15 ">
-          <InfoBox Icon={ShoppingCart}
+          <InfoBox Icon={Coffee}
                    color={pink600}
                    title="Comidas"
-                   value="1500k"
           />
         </div>
 
-
         <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3 m-b-15 ">
-          <InfoBox Icon={ThumbUp}
-                   color={cyan600}
+          <InfoBox Icon={Car}
+                   color={orange600}
                    title="Transporte"
-                   value="4231"
           />
         </div>
 
@@ -41,37 +34,16 @@ const DashboardPage = () => {
           <InfoBox Icon={Assessment}
                    color={purple600}
                    title="Outros"
-                   value="460"
           />
         </div>
 
-        {/* <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3 m-b-15 ">
-          <InfoBox Icon={Face}
-                   color={orange600}
-                   title="New Members"
-                   value="248"
-          />
-        </div> */}
-      </div>
-
-      <div className="row">
-        {/* <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-md m-b-15">
-          <NewOrders data={Data.dashBoardPage.newOrders}/>
-        </div> */}
-
-        {/* <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 m-b-15">
-          <MonthlySales data={Data.dashBoardPage.monthlySales}/>
-        </div> */}
       </div>
 
       <div className="row">
         <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 m-b-15 ">
           <RecentlyProducts data={Data.dashBoardPage.recentProducts}/>
         </div>
-
-        {/* <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 m-b-15 ">
-          <BrowserUsage data={Data.dashBoardPage.browserUsage}/>
-        </div> */}
+ 
       </div>
     </div>
   );
