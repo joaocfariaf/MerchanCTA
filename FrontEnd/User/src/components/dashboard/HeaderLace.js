17b3e2c767c88e6bb793bf3151a7326e222e9555
joao <joaocfariaf@gmail.com>
2019-06-27 01:18:47 -0300

    Again, fixing conflicts

--- FrontEnd/User/src/components/dashboard/HeaderLace.js
+++ FrontEnd/User/src/components/dashboard/HeaderLace.js
@@ -1,120 +1,124 @@
-// import React, {PropTypes} from 'react';
-// import Paper from 'material-ui/Paper';
-// import {grey800} from 'material-ui/styles/colors';
-// // import {typography} from 'material-ui/styles';
+import React, {PropTypes} from 'react';
+import {grey800, transparent} from 'material-ui/styles/colors';
+// import {typography} from 'material-ui/styles';
 
-// class HeaderLace extends React.Component {
+class HeaderLace extends React.Component {
 
-//   render() {
-//     const {title} = this.props;
+  render() {
+    const {title, Moldura} = this.props;
 
-//     const styles = {
-//       content: {
-//         textAlign: 'center',
-//         height: 180,
-//         backgroundImage: "url(" + require("../images/moldura.png") + ")"
-//       },
+    const styles = {
+      content: {
+        textAlign: 'center',
+        height: 180,
+    },
+    moldura: {
+        height: 180,
+        width: 200,
+        margintop: 20,
+        maxWidth: '100%'
+    },
+    text: {
+        fontSize: 80,
+        color: grey800,
+      } 
+    };
 
-//       text: {
-//         fontSize: 80,
-//         color: grey800,
-//       } 
-//     };
+    return (
 
-//     return (
-//       <Paper>
-//         <div style={styles.content}>
-//           <span style={styles.text}>{title}</span>
-//         </div>
-//       </Paper>
-//       );
-//   }
-// }
-
-// HeaderLace.propTypes = {
-//   Icon: PropTypes.any, // eslint-disable-line
-//   color: PropTypes.string,
-//   title: PropTypes.string,
-//   value: PropTypes.string
-// };
-
-// export default HeaderLace;
-
-
-
-// // const LeftDrawer = props => {
-// //     let { navDrawerOpen } = props;
-  
-// //     const styles = {
-// //       logo: {
-// //         cursor: "pointer",
-// //         fontSize: 46,
-// //         color: typography.textDarkBlack,
-// //         lineHeight: `${spacing.desktopKeylineIncrement}px`,
-// //         fontWeight: typography.fontWeightLight,
-// //         backgroundColor: white,
-// //         paddingLeft: 40,
-// //         height: 56
         
-// //       },
-// //       menuItem: {
-// //         color: white,
-// //         fontSize: 14
-// //       },
-// //       avatar: {
-// //         div: {
-// //           padding: "15px 0 20px 15px",
-// //           backgroundImage: "url(" + require("../images/CEU.png") + ")",
-// //           height: 20
-// //         },
-// //         icon: {
-// //           float: "left",
-// //           display: "block",
-// //           marginRight: 15,
-// //           boxShadow: "0px 0px 0px 0px rgba(0,0,0,0.2)"
-// //         },
-// //         span: {
-// //           paddingTop: 12,
-// //           display: "block",
-// //           color: "white",
-// //           fontWeight: 300,
-// //           textShadow: "1px 1px #444",
-// //           textAlign = 'center'
-// //         }
-// //       }
-// //     };
+        <div style={styles.content}>
+          <Moldura
+            style={styles.moldura}
+          />
+          <span style={styles.text}>{title}</span>
+        </div>
+      );
+  }
+}
+
+HeaderLace.propTypes = {
+  title: PropTypes.string,
+  Moldura: PropTypes.any
+};
+
+export default HeaderLace;
+
+
+
+// const LeftDrawer = props => {
+//     let { navDrawerOpen } = props;
   
-// //     return (
-// //       <Drawer docked={true} open={navDrawerOpen}>
-// //         <div style={styles.logo}>MerchanCTA</div>
-// //         <div style={styles.avatar.div}>
-// //           <Avatar
-// //             src="http://www.material-ui.com/images/uxceo-126.jpg"
-// //             size={300}
-// //             style={styles.avatar.icon}
-// //           />
-// //           <span style={styles.avatar.span}>{props.username}</span>
-// //         </div>
-// //         <div>
-// //           {props.menus.map((menu, index) => (
-// //             <MenuItem
-// //               key={index}
-// //               style={styles.menuItem}
-// //               primaryText={menu.text}
-// //               leftIcon={menu.icon}
-// //               containerElement={<Link to={menu.link} />}
-// //             />
-// //           ))}
-// //         </div>
-// //       </Drawer>
-// //     );
-// //   };
+//     const styles = {
+//       logo: {
+//         cursor: "pointer",
+//         fontSize: 46,
+//         color: typography.textDarkBlack,
+//         lineHeight: `${spacing.desktopKeylineIncrement}px`,
+//         fontWeight: typography.fontWeightLight,
+//         backgroundColor: white,
+//         paddingLeft: 40,
+//         height: 56
+        
+//       },
+//       menuItem: {
+//         color: white,
+//         fontSize: 14
+//       },
+//       avatar: {
+//         div: {
+//           padding: "15px 0 20px 15px",
+//           backgroundImage: "url(" + require("../images/CEU.png") + ")",
+//           height: 20
+//         },
+//         icon: {
+//           float: "left",
+//           display: "block",
+//           marginRight: 15,
+//           boxShadow: "0px 0px 0px 0px rgba(0,0,0,0.2)"
+//         },
+//         span: {
+//           paddingTop: 12,
+//           display: "block",
+//           color: "white",
+//           fontWeight: 300,
+//           textShadow: "1px 1px #444",
+//           textAlign = 'center'
+//         }
+//       }
+//     };
   
-// //   LeftDrawer.propTypes = {
-// //     navDrawerOpen: PropTypes.bool,
-// //     menus: PropTypes.array,
-// //     username: PropTypes.string
-// //   };
+//     return (
+//       <Drawer docked={true} open={navDrawerOpen}>
+//         <div style={styles.logo}>MerchanCTA</div>
+//         <div style={styles.avatar.div}>
+//           <Avatar
+//             src="http://www.material-ui.com/images/uxceo-126.jpg"
+//             size={300}
+//             style={styles.avatar.icon}
+//           />
+//           <span style={styles.avatar.span}>{props.username}</span>
+//         </div>
+//         <div>
+//           {props.menus.map((menu, index) => (
+//             <MenuItem
+//               key={index}
+//               style={styles.menuItem}
+//               primaryText={menu.text}
+//               leftIcon={menu.icon}
+//               containerElement={<Link to={menu.link} />}
+//             />
+//           ))}
+//         </div>
+//       </Drawer>
+//     );
+//   };
   
-// //   export default LeftDrawer;
+//   LeftDrawer.propTypes = {
+//     navDrawerOpen: PropTypes.bool,
+//     menus: PropTypes.array,
+//     username: PropTypes.string
+//   };
+  
+//   export default LeftDrawer;
   
--- FrontEnd/User/src/containers/DashboardPage.js
+++ FrontEnd/User/src/containers/DashboardPage.js
@@ -6,7 +6,7 @@ import Coffee from "material-ui/svg-icons/maps/local-cafe";
 import InfoBox from "../components/dashboard/InfoBox";
 import RecentlyProducts from "../components/dashboard/RecentlyProducts";
 import globalStyles from "../styles";
-// import HeaderLace from "../components/dashboard/HeaderLace";
+import HeaderLace from "../components/dashboard/HeaderLace";
 
 class DashboardPage extends React.Component {
   constructor(props) {
@@ -53,6 +53,9 @@ class DashboardPage extends React.Component {
       <div>
         <h3 style={globalStyles.navigation} />
         <div className="row">
+          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4 m-b-15">
+            <HeaderLace title="PAPAPAP" backgroundImage = {"url(" + require("../images/moldura.png") + ")"}/>
+          </div>
         </div>
         
         <div className="row">
