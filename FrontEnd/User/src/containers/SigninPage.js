f3be6cb0e136915747483777d839298069b601e8
Adrisson Samersla <adrissonsamersla@gmail.com>
2019-06-27 02:59:53 -0300

    Not yet finished

--- FrontEnd/User/src/containers/SigninPage.js
+++ FrontEnd/User/src/containers/SigninPage.js
@@ -1,132 +1,8 @@
-// import React, { Component } from "react";
-// import { Link } from "react-router";
-// import RaisedButton from "material-ui/RaisedButton";
-// import TextField from "material-ui/TextField";
-// import { grey400 } from "material-ui/styles/colors";
-// //import Divider from "material-ui/Divider";
-// import PageBase from "../components/PageBase";
-
-// const styles = {
-//   toggleDiv: {
-//     maxWidth: 300,
-//     marginTop: 40,
-//     marginBottom: 5
-//   },
-//   toggleLabel: {
-//     color: grey400,
-//     fontWeight: 100
-//   },
-//   buttons: {
-//     marginTop: 30,
-//     float: "right"
-//   },
-//   saveButton: {
-//     marginLeft: 5
-//   }
-// };
-
-// class SigninPage extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.inputsInfo = {
-//       name: "",
-//       description: ""
-//     };
-//   }
-
-//   makingRequest(event) {
-//     event.preventDefault();
-//     // console.log(this.inputsInfo);
-
-//     const requestInfo = {
-//       method: "POST",
-//       body: JSON.stringify({
-//         name: this.inputsInfo.name,
-//         description: this.inputsInfo.description
-//       }),
-//       headers: new Headers({
-//         "Content-type": "application/json"
-//       })
-//     };
-
-//     fetch("https://ces22-backend.herokuapp.com/registration", requestInfo)
-//       .then(response => {
-//         if (response.ok) {
-//           alert("Rapaz, só não deu é pouco");
-//           return response.text();
-//         } else {
-//           throw new Error("não foi possível fazer o login");
-//         }
-//       })
-//       .catch(error => {
-//         alert("Deu errado");
-//         console.log(error.message);
-//         //this.setState({msg:error.message});
-//       });
-//   }
-
-//   handleChange(tag) {
-//     return event => {
-//       this.inputsInfo[tag] = event.target.value;
-//     };
-//   }
-
-//   render() {
-//     return (
-//       <PageBase title="Adicionar Produto" navigation="Admin / Forms">
-//         <form onSubmit={this.makingRequest.bind(this)}>
-//           <TextField
-//             hintText="Nome"
-//             floatingLabelText="Nome"
-//             fullWidth={true}
-//             onChange={this.handleChange("name")}
-//           />
-//           <TextField
-//             id="filled-number"
-//             label="Number"
-//             floatingLabelText="Preço"
-//             hintText="Exemplo: 10.23"
-//             InputLabelProps={{
-//               shrink: true
-//             }}
-//             margin="normal"
-//             variant="filled"
-//           />
-//           <TextField
-//             hintText="Descrição"
-//             floatingLabelText="Descrição"
-//             fullWidth={true}
-//             onChange={this.handleChange("description")}
-//           />
-//           <div style={styles.buttons}>
-//             <Link to="/">
-//               <RaisedButton label="Cancelar" />
-//             </Link>
-
-//             <RaisedButton
-//               label="Salvar"
-//               style={styles.saveButton}
-//               type="submit"
-//               primary={true}
-//             />
-//           </div>
-//         </form>
-//       </PageBase>
-//     );
-//   }
-// }
-
-// export default SigninPage;
-
 import React, { Component } from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import Paper from "material-ui/Paper";
 import RaisedButton from "material-ui/RaisedButton";
-import FlatButton from "material-ui/FlatButton";
 import { blue900, white } from "material-ui/styles/colors";
-import PersonAdd from "material-ui/svg-icons/social/person-add";
-import Help from "material-ui/svg-icons/action/help";
 import TextField from "material-ui/TextField";
 import { browserHistory, Link } from "react-router";
 import ThemeDefault from "../theme-default";
@@ -213,7 +89,7 @@ class SigninPage extends Component {
       })
     };
 
-    fetch("https://ces22-backend.herokuapp.com/login", requestInfo)
+    fetch("https://ces22-backend.herokuapp.com/registration", requestInfo)
       .then(res => res.json())
       .then(json => {
         localStorage.setItem("MerchanCTA-UserTokens", json.access_token);
@@ -232,12 +108,6 @@ class SigninPage extends Component {
         });
       });
   }
-  // .catch(error => {
-  //   this.setState({
-  //     msg_user:
-  //       "Não foi possível realizar o login. Motivo: " + error.message
-  //   });
-  // });
 
   handleChange(tag) {
     return event => {
@@ -251,6 +121,7 @@ class SigninPage extends Component {
         <div>
           <div style={styles.loginContainer}>
             <Paper style={styles.paper}>
+              <h1>Welcome</h1>
               <form onSubmit={this.makingRequest.bind(this)}>
                 <TextField
                   hintText="E-mail"
@@ -265,12 +136,19 @@ class SigninPage extends Component {
                   type="password"
                   onChange={this.handleChange("password")}
                 />
+                <TextField
+                  hintText="Confirm Password"
+                  floatingLabelText="Confirm Password"
+                  fullWidth={true}
+                  type="password"
+                  onChange={this.handleChange("password")}
+                />
 
                 <div>
                   <span>{this.state.msg_user}</span>
                   <Link>
                     <RaisedButton
-                      label="Login"
+                      label="Sign In"
                       primary={true}
                       type="submit"
                       style={styles.loginBtn}
@@ -280,7 +158,7 @@ class SigninPage extends Component {
               </form>
             </Paper>
 
-            <div style={styles.buttonsDiv}>
+            {/* <div style={styles.buttonsDiv}>
               <FlatButton
                 label="Register"
                 href="/SigninPage.js"
@@ -294,7 +172,7 @@ class SigninPage extends Component {
                 style={styles.flatButton}
                 icon={<Help />}
               />
-            </div>
+            </div> */}
           </div>
         </div>
       </MuiThemeProvider>
